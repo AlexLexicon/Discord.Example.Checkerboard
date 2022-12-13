ce42d2a4edf9d17d6e45cd8b74f763ccc06fea22
Alex Stroot <alexrs.business@outlook.com>
2022-12-13 11:32:34 -0800

    added programmatic grid

---
+++ Discord.Example.Checkerboard.WPF/FromXamlUserControl.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System.Windows.Controls;
+using System.Windows.Media;
+
+namespace Discord.Example.Checkerboard.WPF;
+public partial class FromXamlUserControl : UserControl
+{
+    public FromXamlUserControl()
+    {
+        InitializeComponent();
+
+        for (int index = 0; index < BorderGrid.Children.Count; index++)
+        {
+            if (BorderGrid.Children[index] is Border border)
+            {
+                Color color;
+
+                if (index % 2 == 0)
+                {
+                    //odd color
+                    color = Colors.Black;
+                }
+                else
+                {
+                    //even color
+                    color = Colors.Red;
+                }
+
+                border.Background = new SolidColorBrush(color);
+            }
+        }
+    }
+}
---
+++ Discord.Example.Checkerboard.WPF/ProgrammaticallyUserControl.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System.Windows.Controls;
+using System.Windows.Media;
+
+namespace Discord.Example.Checkerboard.WPF;
+public partial class ProgrammaticallyUserControl : UserControl
+{
+    public ProgrammaticallyUserControl()
+    {
+        InitializeComponent();
+
+        int totalColumns = 8;
+        int totalRows = 8;
+
+        for (int row = 1; row <= totalRows; row++)
+        {
+            BorderGrid.ColumnDefinitions.Add(new ColumnDefinition());
+            BorderGrid.RowDefinitions.Add(new RowDefinition());
+
+            for (int column = 1; column <= totalColumns; column++)
+            {
+                Brush color;
+
+                if ((column + row) % 2 == 0)
+                {
+                    color = Brushes.White;
+                }
+                else
+                {
+                    color = Brushes.Black;
+                }
+
+                var border = new Border
+                {
+                    Width = 50,
+                    Height = 50,
+                    Background = color,
+                };
+
+                Grid.SetColumn(border, column - 1);
+                Grid.SetRow(border, row - 1);
+
+                BorderGrid.Children.Add(border);
+            }
+        }
+    }
+}
